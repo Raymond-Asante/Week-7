f42b4f554a2a4ac16cacd8fecdd60c0422faab29
Raymond-Asante <RaymondWins228@outlook.com>
2025-10-14 19:09:12 -0400

    updated commit

---
+++ js/script.js
@@ -0,0 +1,50 @@
+/* var name = "Raymond";
+ let age = 19;
+ let age = prompt("Enter your age:", 19);
+console.log("My name is " + name);
+
+console.log("My age is " + age);
+
+for (var i =0; i<10; i++){
+    console.log("The value of i inside is" + i);
+    if (i == 5){
+        break
+    }
+}
+console.log("The value of i outside is" + i);
+if ( age >18){
+    alert("you are an adult");
+}else {
+        alert("you are a minor");
+    }
+if (isNaN(userEntry) || userEntry == "")
+    alert("Invalid Entry");
+else
+    alert("You entered " + userEntry); 
+
+//writes on webpage ( 0, 2, 4, 6, 8, 10)
+for ( let i = 0; i <= 10; i += 2){
+    document.write(i + " ");
+}
+*/
+/*
+var repeat = "y";
+do{
+var miles = 0;
+do {
+     prompt("Enter Miles", 20);
+}while( isNaN(miles) || miles <= 0);
+
+var gallons = 0;
+do{
+     prompt("Enter Gallons", 2);
+}while( isNaN(gallons) || gallons <= 0);
+ prompt("Enter Gallons", 2);
+
+ var mpg = miles / gallons;
+
+ alert ("Your car mileage is " + mpg + "miles per gallon");
+ repeat = prompt("Do you want to calculate again? (y/n)", "y");
+}while(repeat.toLowerCase() == "y");
+document.writeIn(`<p> Your car mileage is ${mpg} miles per gallon<p>`);
+*/
